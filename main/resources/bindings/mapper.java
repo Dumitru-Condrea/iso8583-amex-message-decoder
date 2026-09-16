3992473be0104514c61114c010b48c5438381dbf
Dumitru Condrea <dumitru.condrea.projects@gmail.com>
2026-09-16 18:07:25 +0300

    Update mapper.java

--- main/resources/bindings/mapper.java
+++ main/resources/bindings/mapper.java
@@ -1,21 +1,34 @@
+package your.package.api.client;
+
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public final class ApiResponseMapper {
 
     private ApiResponseMapper() {
     }
 
-    public static ApiResponse from(Response response) {
+    public static ApiResponse from(
+            Response response) {
 
-        Map<String, List<String>> headers = response
-                .headers()
-                .asList()
-                .stream()
-                .collect(Collectors.groupingBy(
-                        Header::getName,
-                        Collectors.mapping(
-                                Header::getValue,
-                                Collectors.toList()
-                        )
-                ));
+        Map<String, List<String>> headers =
+                response
+                        .headers()
+                        .asList()
+                        .stream()
+                        .collect(
+                                Collectors.groupingBy(
+                                        Header::getName,
+                                        Collectors.mapping(
+                                                Header::getValue,
+                                                Collectors.toList()
+                                        )
+                                )
+                        );
 
         return new ApiResponse(
                 response.statusCode(),
