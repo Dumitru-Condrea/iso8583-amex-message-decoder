67adc3db5c6f4814653dc1e895a1c3d57e80a6e6
Dumitru Condrea <dumitru.condrea.projects@gmail.com>
2026-09-21 15:35:14 +0300

    Update emvAuth.java

--- main/resources/bindings/emvAuth.java
+++ main/resources/bindings/emvAuth.java
@@ -1,18 +1,9 @@
-package your.package.api.auth.service;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import your.package.api.auth.client.AuthClient;
-import your.package.api.client.RestClient;
-import your.package.api.config.ApiConfig;
-import your.package.api.config.ServiceUrls;
-import your.package.api.json.JsonUtils;
-
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.UUID;
 
 public final class EmvAuthService extends AuthService {
+
+    private static final String IDENTITY_REQUEST =
+            "requests/auth/emv-identity.json";
 
     public EmvAuthService(RestClient restClient) {
         super(
@@ -36,49 +27,21 @@ public final class EmvAuthService extends AuthService {
     @Override
     protected JsonNode createIdentityBody() {
 
-        String subUuid = UUID.randomUUID()
-                .toString()
-                .substring(0, 10);
-
-        ObjectNode body =
-                JsonUtils.objectNode();
+        ObjectNode body = (ObjectNode) JsonUtils.readResource(
+                IDENTITY_REQUEST
+        );
 
         body.put(
                 "identityName",
-                "id-for-emv-" + subUuid
-        );
-
-        body.put(
-                "description",
-                "Sample Description"
-        );
-
-        body.set(
-                "scopes",
-                JsonUtils.toJsonNode(
-                        Arrays.asList(
-                                "emvtok.create",
-                                "emvtok.read",
-                                "emvtok.update",
-                                "emvtok.delete"
-                        )
-                )
-        );
-
-        body.set(
-                "resources",
-                JsonUtils.toJsonNode(
-                        Collections.singletonList(
-                                "http://tokenization-gateway/"
-                        )
-                )
-        );
-
-        body.set(
-                "additionalInfo",
-                JsonUtils.objectNode()
+                "id-for-emv-" + generateSubUuid()
         );
 
         return body;
+    }
+
+    private String generateSubUuid() {
+        return UUID.randomUUID()
+                .toString()
+                .substring(0, 10);
     }
 }
